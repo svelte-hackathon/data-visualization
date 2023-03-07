33d01445eb2ab437a6853c5cc1f47d1abe30ac76
Jake Collins <shinsina@att.net>
2023-03-07 15:12:08 -0600

    Remove unused borderSkipChecks utility function

--- src/lib/utils/border-skip-check.ts
+++
@@ -1,8 +0,0 @@
-import { BorderSkips } from '$lib/types/bar-chart';
-
-export default function borderskipCheck(value: string): BorderSkips {
-  if (Object.values(BorderSkips).includes(value as BorderSkips)) {
-    return value as BorderSkips;
-  }
-  return BorderSkips.false;
-}
