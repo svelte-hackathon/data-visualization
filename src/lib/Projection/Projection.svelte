e36a2bbba3556a469103f0c683393c4ef4d94460
Bert Bengtson <robert.bengtson@snc.edu>
2023-03-15 19:01:42 -0400

    Convert Projection to Svg

--- src/lib/Projection/Projection.svelte
+++ src/lib/Projection/Projection.svelte
@@ -2,71 +2,42 @@
   import {
     geoPath,
     geoOrthographic,
-    type GeoPermissibleObjects,
-    type ExtendedFeatureCollection,
     geoCentroid,
-    geoInterpolate
+    geoInterpolate,
+    type GeoPermissibleObjects,
+    type ExtendedFeatureCollection
   } from 'd3-geo';
-  import { tweened } from 'svelte/motion';
-  import { afterUpdate } from 'svelte';
+
+  import { tween } from './tween';
   import { feature } from 'topojson-client';
   import { Versor } from './Versor';
 
   export let width = 720;
   export let tilt = 20;
-
   export let name = ''; //name of country
-
   export let world: TopoJSON.Topology;
-
-  let land: GeoPermissibleObjects | null;
-  let countryGraphic: GeoPermissibleObjects | null = null;
-  let ref: HTMLCanvasElement;
-  let projection = geoOrthographic();
-  let arcCompleted = false;
 
   const sphere: GeoPermissibleObjects = { type: 'Sphere' };
 
-  function render(land: GeoPermissibleObjects | null, ref: HTMLCanvasElement) {
-    if (!ref) {
-      return;
-    }
-
-    const context = ref.getContext('2d');
-
-    if (!context) {
-      console.error('NO Context');
-      return;
-    }
-
-    if (!land) {
-      console.error('NO LAND');
-      return;
-    }
-
-    const path = geoPath(projection, context);
-
-    context.clearRect(0, 0, width, height);
-
-    context.beginPath(), path(sphere), (context.fillStyle = '#fff'), context.fill();
-
-    context.beginPath(), path(land), (context.fillStyle = '#000'), context.fill();
-
-    context.beginPath(), path(sphere), context.stroke();
-
-    if (countryGraphic) {
-      context.beginPath(), path(countryGraphic), (context.fillStyle = '#f00'), context.fill();
-    }
-    if (arc) {
-      context.beginPath(), path(arc), context.stroke();
-    }
-  }
+  let land: GeoPermissibleObjects | null;
+  let countryGraphic: GeoPermissibleObjects | null = null;
+  let arc: GeoPermissibleObjects | null = null;
+  let projection = geoOrthographic();
+  let position1: [number, number] = [0, 0];
+  let position2: [number, number] = [0, 0];
+  let r1 = [0, 0, 0];
+  let r2 = [0, 0, 0];
+  let interpolatePosition: (t: number) => [number, number];
+  let interpolateVersor: (t: any) => number[];
+  let inAnimation: Promise<void> | null = null;
+  let outAnimation: Promise<void> | null = null;
 
   function getHeight(width: number) {
     const [[x0, y0], [x1, y1]] = geoPath(projection.fitWidth(width, sphere)).bounds(sphere);
     const dy = Math.ceil(y1 - y0);
     const l = Math.min(Math.ceil(x1 - x0), dy);
     projection.scale((projection.scale() * (l - 1)) / l).precision(0.2);
+    projection = projection;
     return dy;
   }
 
@@ -82,46 +53,69 @@
     return [];
   }
 
-  let p1:[number, number], p2: [number, number] = [0, 0], r1, r2 = [0, 0, 0];
-  let ip = () => [];
-  let iv = () => [];
-  function worldTour(countryName: string) {
+  async function worldTour(countryName: string) {
+    //see if an animation is currently running
+    if (inAnimation) await inAnimation;
+    if (outAnimation) await outAnimation;
+    if (countryName != name) return; //Cancel old animations if people switch before it can complete
+
     const country = countries.find((country) => country?.properties?.name === countryName);
 
     if (!country) return;
 
     countryGraphic = country;
-    arcCompleted = false;
 
-    p1 = p2, p2 = geoCentroid(countryGraphic);
-    r1 = r2, r2 = [-p2[0], tilt - p2[1], 0];
+    (position1 = position2), (position2 = geoCentroid(countryGraphic));
+    (r1 = r2), (r2 = [-position2[0], tilt - position2[1], 0]);
 
-    ip = geoInterpolate(p1, p2);
-    iv = Versor.interpolateAngles(r1, r2);
-    $t = 1;
+    interpolatePosition = geoInterpolate(position1, position2);
+    interpolateVersor = Versor.interpolateAngles(r1, r2);
+
+    inAnimation = tween.set(1);
+    requestAnimationFrame(inAnimationFunc);
+    await inAnimation;
+    inAnimation = null;
+
+    outAnimation = tween.set(0);
+    interpolatePosition = geoInterpolate(position2, position1);
+    requestAnimationFrame(outAnimationFunc);
+    await outAnimation;
+    outAnimation = null;
+    arc = null;
   }
 
-  const t = tweened(0, {
-    duration: 1250
-  });
+  const inAnimationFunc = () => {
+    if (!inAnimation) return;
+    projection.rotate(interpolateVersor($tween));
+    projection = projection;
+    arc = { type: 'LineString', coordinates: [position1, interpolatePosition($tween)] };
+    requestAnimationFrame(inAnimationFunc);
+  };
+
+  const outAnimationFunc = () => {
+    if (!outAnimation) return;
+    arc = { type: 'LineString', coordinates: [interpolatePosition($tween), position2] };
+    requestAnimationFrame(outAnimationFunc);
+  };
 
   $: land = world ? feature(world, world.objects.land) : null;
   $: height = getHeight(width);
   $: countries = getCountries(world);
   $: worldTour(name);
-
-  $: projection.rotate(arcCompleted ? iv(1) as [number, number, number] : iv($t) as [number, number, number]);
-  $: arc = { type: 'LineString', coordinates: [p1, ip($t)] };
-
-  $: if($t === 1) {
-    arcCompleted = true;
-    $t = 0;
-  }
- 
-  afterUpdate(() => {
-    render(land, ref);
-  });
+  $: path = geoPath(projection);
 </script>
 
-<canvas {width} {height} bind:this={ref} />
-
+<svg {width} {height} viewBox="0 0 {width} {height}">
+  {#if land}
+    <g>
+      <path class="sphere" d={path(sphere)} stroke="#ccc" fill="white" />
+      <path class="land" d={path(land)} stroke="none" fill="#ccc" />
+      {#if countryGraphic}
+        <path class="selected" d={path(countryGraphic)} fill="red" />
+      {/if}
+      {#if arc}
+        <path class="arc" d={path(arc)} stroke="black" stroke-width="2" fill="none" />
+      {/if}
+    </g>
+  {/if}
+</svg>
---
+++ src/lib/Projection/tween.ts
@@ -0,0 +1,5 @@
+import { tweened } from "svelte/motion";
+
+export const tween = tweened(0, {
+    duration: 1500
+});
