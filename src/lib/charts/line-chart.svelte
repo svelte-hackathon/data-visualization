f49efb97cc4f3e98dbf6697476fbc76cf4ae1319
Shinsina <shinsina@att.net>
2023-03-17 01:32:18 +0000

    Initial version of the LineChart component

---
+++ src/lib/charts/line-chart.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  import type { ChartData, ChartOptions, ChartItem } from 'chart.js';
+  import Chart from './chart.svelte';
+  export let data: ChartData<'line'>,
+    options: ChartOptions<'line'>,
+    chartID: ChartItem;
+</script>
+
+<Chart type="line" {data} {options} {chartID} />
