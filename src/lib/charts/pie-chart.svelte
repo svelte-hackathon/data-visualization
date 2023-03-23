7dca4964b0edcd465d30379158355086c7b337b6
Shinsina <shinsina@att.net>
2023-03-23 00:27:45 +0000

    Initial version of PieChart component

---
+++ src/lib/charts/pie-chart.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  import type { ChartData, ChartOptions, ChartItem } from 'chart.js';
+  import Chart from './chart.svelte';
+  export let data: ChartData<'pie'>,
+    options: ChartOptions<'pie'>,
+    chartID: ChartItem;
+</script>
+
+<Chart type="pie" {data} {options} {chartID} />
