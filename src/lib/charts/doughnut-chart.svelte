688beb37bc421afdd6027721ac0d6b90905462c6
Shinsina <shinsina@att.net>
2023-03-23 00:28:07 +0000

    Initial version of DoughnutChart component

---
+++ src/lib/charts/doughnut-chart.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  import type { ChartData, ChartOptions, ChartItem } from 'chart.js';
+  import Chart from './chart.svelte';
+  export let data: ChartData<'doughnut'>,
+    options: ChartOptions<'doughnut'>,
+    chartID: ChartItem;
+</script>
+
+<Chart type="doughnut" {data} {options} {chartID} />
