a402d3e4f09559c8f65e722768261d4c6518c860
Shinsina <shinsina@att.net>
2023-03-23 00:42:08 +0000

    Initial version of PolarAreaChart component

---
+++ src/lib/charts/polar-area-chart.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  import type { ChartData, ChartOptions, ChartItem } from 'chart.js';
+  import Chart from './chart.svelte';
+  export let data: ChartData<'polarArea'>,
+    options: ChartOptions<'polarArea'>,
+    chartID: ChartItem;
+</script>
+
+<Chart type="polarArea" {data} {options} {chartID} />
