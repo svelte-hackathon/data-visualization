6e88272fae4c3ef79b5fe89095545b0c34fad79e
Shinsina <shinsina@att.net>
2023-03-23 00:50:46 +0000

    Utilize RadarChart component on /radar-chart page

---
+++ src/routes/radar-chart/+page.svelte
@@ -0,0 +1,50 @@
+<script lang="ts">
+  import RadarChart from '$lib/charts/radar-chart.svelte';
+  import {
+    Chart as ChartJS,
+    RadarController,
+    LineElement,
+    PointElement,
+    RadialLinearScale,
+    Title,
+    Legend,
+    Filler,
+    type ChartData,
+    type ChartOptions,
+  } from 'chart.js';
+  ChartJS.register(
+    RadarController,
+    LineElement,
+    PointElement,
+    RadialLinearScale,
+    Filler,
+    Title,
+    Legend
+  );
+
+  const data: ChartData<'radar'> = {
+    labels: ['Increasing Y', 'Increasing X', 'Decreasing X'],
+    datasets: [
+      {
+        data: [404, 301, 200],
+        label: 'Scanning For Data...',
+        backgroundColor: 'yellow'
+      },
+      {
+        data: [301, 200, 404],
+        label: 'Scanning For Data...',
+        backgroundColor: 'green'
+      },
+      {
+        data: [200, 404, 301],
+        label: 'Scanning For Data...',
+        backgroundColor: 'red'
+      }
+    ]
+  };
+
+  const options: ChartOptions<'radar'> = {};
+
+</script>
+
+<RadarChart {data} {options} chartID="radar-chart-1" />
