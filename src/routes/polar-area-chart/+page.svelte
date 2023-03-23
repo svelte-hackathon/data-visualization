5c36ae6cd6bf314006ea18d9614a531853af3b4b
Shinsina <shinsina@att.net>
2023-03-23 00:42:44 +0000

    Use PolarAreaChart component on /polar-area-chart

---
+++ src/routes/polar-area-chart/+page.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+  import PolarAreaChart from '$lib/charts/polar-area-chart.svelte';
+  import {
+    Chart as ChartJS,
+    PolarAreaController,
+    ArcElement,
+    RadialLinearScale,
+    Title,
+    Legend,
+    Filler,
+    type ChartData,
+    type ChartOptions,
+  } from 'chart.js';
+  ChartJS.register(
+    PolarAreaController,
+    ArcElement,
+    RadialLinearScale,
+    Filler,
+    Title,
+    Legend
+  );
+
+  const data: ChartData<'polarArea'> = {
+    labels: ['Yellow Slice', 'Green Slice', 'Red Slice'],
+    datasets: [
+      {
+        data: [404, 301, 200],
+        label: 'Polar Area Chart Number One',
+        backgroundColor: ['yellow', 'green', 'red']
+      }
+    ]
+  };
+
+  const options: ChartOptions<'polarArea'> = {
+    animation: {
+      animateRotate: false,
+    },
+  }
+
+</script>
+
+<PolarAreaChart {data} {options} chartID="pie-chart-1" />
