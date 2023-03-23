e222fbaae247c849c22e661124280fe502f83121
Shinsina <shinsina@att.net>
2023-03-23 00:59:23 +0000

    Revise "Area Chart" component definitions.

--- src/lib/charts/doughnut-chart.svelte
+++ src/lib/charts/doughnut-chart.svelte
@@ -1,9 +1,37 @@
 <script lang="ts">
-  import type { ChartData, ChartOptions, ChartItem } from 'chart.js';
-  import Chart from './chart.svelte';
+  import { onMount, afterUpdate, onDestroy } from 'svelte';
+  import { Chart as ChartJS } from 'chart.js';
+  import type {
+    ChartData,
+    ChartOptions,
+    ChartItem,
+    ChartConfiguration
+  } from 'chart.js';
+
   export let data: ChartData<'doughnut'>,
     options: ChartOptions<'doughnut'>,
     chartID: ChartItem;
+
+  let ref: HTMLCanvasElement;
+  let chart: ChartJS<'doughnut'> | null;
+  const config: ChartConfiguration<'doughnut'> = {
+    type: 'doughnut',
+    data,
+    options
+  };
+  const id = String(chartID);
+  onMount(() => {
+    chart = new ChartJS(ref, config);
+  });
+  afterUpdate(() => {
+    if (!chart) return;
+    chart.data = data;
+    Object.assign(chart.options, options);
+  });
+  onDestroy(() => {
+    if (chart) chart.destroy();
+    chart = null;
+  });
 </script>
 
-<Chart type="doughnut" {data} {options} {chartID} />
+<canvas bind:this={ref} {id} />
--- src/lib/charts/pie-chart.svelte
+++ src/lib/charts/pie-chart.svelte
@@ -1,9 +1,37 @@
 <script lang="ts">
-  import type { ChartData, ChartOptions, ChartItem } from 'chart.js';
-  import Chart from './chart.svelte';
+  import { onMount, afterUpdate, onDestroy } from 'svelte';
+  import { Chart as ChartJS } from 'chart.js';
+  import type {
+    ChartData,
+    ChartOptions,
+    ChartItem,
+    ChartConfiguration
+  } from 'chart.js';
+
   export let data: ChartData<'pie'>,
     options: ChartOptions<'pie'>,
     chartID: ChartItem;
+
+  let ref: HTMLCanvasElement;
+  let chart: ChartJS<'pie'> | null;
+  const config: ChartConfiguration<'pie'> = {
+    type: 'pie',
+    data,
+    options
+  };
+  const id = String(chartID);
+  onMount(() => {
+    chart = new ChartJS(ref, config);
+  });
+  afterUpdate(() => {
+    if (!chart) return;
+    chart.data = data;
+    Object.assign(chart.options, options);
+  });
+  onDestroy(() => {
+    if (chart) chart.destroy();
+    chart = null;
+  });
 </script>
 
-<Chart type="pie" {data} {options} {chartID} />
+<canvas bind:this={ref} {id} />
